f22e16456540ebbf170b7d0e6cf6cc4515af1611
liam_port <liamparker100@outlook.com>
2021-10-16 12:48:55 +0100

    z

--- chess1/chess1/Program.cs
+++ chess1/chess1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace chess1
 {
@@ -6,10 +7,21 @@ namespace chess1
     {
         static void Main(string[] args)
         {
-            string FEN_StartPostition = "4Qr2/b1p1np1p/pPP5/P5KP/1kNRPPPp/1qpr2RN/n3pP1p/4BB1b";
-            chessBoard Board = new chessBoard(FEN_StartPostition);
-            Board.Draw();
+            Genral();
             Console.ReadKey();
+        }
+        static void Genral()
+        {
+            string FEN_StartPostition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+            chessBoard Board = new chessBoard(FEN_StartPostition);
+            while (true)
+            {
+                Board.Draw();
+                Console.Write("square>>>");
+                string square = Console.ReadLine();
+                int[] ValuesXY = Board.ConvertSquareToXY(square);
+                Console.WriteLine("The Piece is a: "+Board.GetPiceFromXY(ValuesXY));
+            }
         }
     }
 
@@ -45,17 +57,39 @@ namespace chess1
         }
         public void Draw()
         {
+            int lineNumber = 1;
+            Console.Write("\n_________________\n");
             for (int i = 0; i < allPices.Length; i++)
             {
                 if ((i + 1) % 8 == 0)
                 {
-                    Console.Write(allPices[i] + "\n");
+                    Console.Write("|" + allPices[i] + "|" + lineNumber.ToString() + "\n_________________\n");
+                    lineNumber++;
                 }
                 else
                 {
-                    Console.Write(allPices[i]);
+                    Console.Write("|" + allPices[i]);
                 }
             }
+            Console.WriteLine(" a b c d e f g h");
+        }
+
+        public int[] ConvertSquareToXY(string square)
+        {
+            int[] ValuesXY = new int[2];
+            int y;
+            char[] col = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+            int.TryParse(square[1].ToString(), out ValuesXY[1]);
+            ValuesXY[0] = Array.IndexOf(col, square[0]) + 1;
+            return ValuesXY;
+
+        }
+
+        public string GetPiceFromXY(int[] ValuesXY)
+        {
+            int Coord = ((ValuesXY[1] - 1) * 8) + ValuesXY[0];
+            string piece = allPices[Coord - 1];
+            return piece;
         }
     }
 
